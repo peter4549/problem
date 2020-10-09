3f5a91753c2e2a29f38305b49b372a7edd712695
peter4549 <42578653+peter4549@users.noreply.github.com>
2020-10-10 07:14:11 +0900

    Create 4796.py

---
+++ Baekjoon Online Judge/4796.py
@@ -0,0 +1,10 @@
+i = 1
+
+while True:
+    L, P, V = map(int, input().split())
+    if L == 0 and P == 0 and V == 0:
+        break
+
+    print(f'Case {i:d}: {(int(V / P) * L + min((V % P), L)):d}')
+    i += 1
+   
