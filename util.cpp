50bef412e8ddea8f3240b0c6f3647c7a834355a6
peter4549 <42578653+peter4549@users.noreply.github.com>
2021-02-04 15:17:40 +0900

    Create util.cpp

---
+++ util.cpp
@@ -0,0 +1,13 @@
+int matrix[n][n];
+
+void rotate() {
+	for (int i = 0; i < n / 2; i++) {
+		for (int j = i; j < n - i - 1; j++) {
+			int k = matrix[i][j];
+			matrix[i][j] = matrix[n - 1 - j][i];
+			matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+			matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+			matrix[j][n - 1 - i] = k;
+		}
+	}
+}
